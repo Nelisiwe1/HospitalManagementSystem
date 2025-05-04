0e9c7dbf200d4542ad6e9fbaa1ba8228f5b82313
Nelisiwe Ngqeme <nelisiwengqeme3@gmail.com>
2025-05-04 22:03:19 +0200

    last commit

--- HospitalTests/obj/Debug/net9.0/HospitalTests.AssemblyInfo.cs
+++ HospitalTests/obj/Debug/net9.0/HospitalTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e21542d8301454bc2db9dd72a61da2eaf9c1716")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df0c3af78e0cd270bbab92a15ebc5146e473b814")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
